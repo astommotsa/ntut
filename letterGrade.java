90a809e85814c8707c05abfe92f0e33f6b6ba310
aaa71 <aaa710.3@gmail.com>
2023-01-13 17:29:18 +0800

    add main

--- letterGrade.java
+++ letterGrade.java
@@ -1,24 +1,31 @@
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+public static char letterGrade(int score) {
+    char grade;
+    if (score <0 || score > 100)
+        grade = 'X';
+    else if (score>=90 && score <=100)
+        grade = 'A';
+    else if (score>=80 && score <90)
+        grade = 'B';
+    else if (score>=70 && score <80)
+        grade = 'C';
+    else if (score>=60 && score <70)
+        grade = 'D';
+    else
+        grade ='F';
+    return grade;
+}
 
-// define the tests we want to run
-
-
-public class SimpleTest {    
-    public static char letterGrade(int score) {
-        char grade;
-        if (score <0 || score > 100)
-            grade = 'X';
-        else if (score>=90 && score <=100)
-            grade = 'A';
-        else if (score>=80 && score <90)
-            grade = 'B';
-        else if (score>=70 && score <80)
-            grade = 'C';
-        else if (score>=60 && score <70)
-            grade = 'D';
-        else
-            grade ='F';
-        return grade;
-    }
+public static void main(String[] args) {
+	System.out.print("Enetr the score = ");
+	try {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		score = Integer.parseInt(br.readLine());
+ 		char grade = letterGrade(score);
+ 		System.out.println("The grade of " + score + " is " + grade);
+	} catch (NumberFormatException ex) {
+		System.out.println("Not an integer!");
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
 }
