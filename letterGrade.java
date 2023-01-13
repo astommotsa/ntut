589d6d52ba7a252756c562f1f5c31ee4b4058b40
aaa71 <aaa710.3@gmail.com>
2023-01-13 17:43:32 +0800

    add test

--- letterGrade.java
+++ letterGrade.java
@@ -1,3 +1,6 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public static char letterGrade(int score) {
     char grade;
     if (score <0 || score > 100)
@@ -26,6 +29,35 @@ public static void main(String[] args) {
 	} catch (NumberFormatException ex) {
 		System.out.println("Not an integer!");
 	} catch (IOException e) {
+		e.printStackTrace();
+	}
+
+	try {
+		assertEquals(letterGrade(95), 'A');
+        assertEquals(letterGrade(85), 'B');
+        assertEquals(letterGrade(75), 'C');
+        assertEquals(letterGrade(65), 'D');
+        assertEquals(letterGrade(55), 'F');
+        assertEquals(letterGrade(-5), 'X');
+        assertEquals(letterGrade(105), 'X');
+	}catch (IOException e) {
+		e.printStackTrace();
+	}
+
+	try {
+        assertEquals(letterGrade(-1), 'X');
+        assertEquals(letterGrade(0), 'F');
+        assertEquals(letterGrade(59), 'F');
+        assertEquals(letterGrade(60), 'D');
+        assertEquals(letterGrade(69), 'D');
+        assertEquals(letterGrade(70), 'C');
+        assertEquals(letterGrade(79), 'C');
+        assertEquals(letterGrade(80), 'B');
+        assertEquals(letterGrade(89), 'B');
+        assertEquals(letterGrade(90), 'A');
+        assertEquals(letterGrade(100), 'A');
+        assertEquals(letterGrade(101), 'X');
+	}catch (IOException e) {
 		e.printStackTrace();
 	}
 }
