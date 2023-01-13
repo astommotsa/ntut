b4f87e1068ec8d5d0b09683741a9a615b35085c8
aaa71 <aaa710.3@gmail.com>
2023-01-13 17:22:42 +0800

    letterGrade

---
+++ letterGrade.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+// define the tests we want to run
+
+
+public class SimpleTest {    
+    public static char letterGrade(int score) {
+        char grade;
+        if (score <0 || score > 100)
+            grade = 'X';
+        else if (score>=90 && score <=100)
+            grade = 'A';
+        else if (score>=80 && score <90)
+            grade = 'B';
+        else if (score>=70 && score <80)
+            grade = 'C';
+        else if (score>=60 && score <70)
+            grade = 'D';
+        else
+            grade ='F';
+        return grade;
+    }
+}
